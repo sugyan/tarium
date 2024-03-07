b242c4bf44153c210093c7947f05b16ae3f35a05
sugyan <sugi1982@gmail.com>
2024-03-07 10:52:46 +0900

    Add sign_in form

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,43 @@
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use atrium_api::agent::{store::MemorySessionStore, AtpAgent};
+use atrium_xrpc_client::reqwest::ReqwestClient;
+use serde::Serialize;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error(transparent)]
+    CreateSession(
+        #[from]
+        atrium_api::xrpc::error::Error<atrium_api::com::atproto::server::create_session::Error>,
+    ),
+}
+
+impl Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_str())
+    }
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn sign_in(
+    identifier: &str,
+    password: &str,
+) -> Result<atrium_api::com::atproto::server::create_session::Output, Error> {
+    let agent = AtpAgent::new(
+        ReqwestClient::new("https://bsky.social"),
+        MemorySessionStore::default(),
+    );
+    Ok(agent.login(identifier, password).await?)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![sign_in])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,51 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const signin = async () => {
+    try {
+      const result = await invoke("sign_in", { identifier, password });
+      setMessage(JSON.stringify(result));
+    } catch (err) {
+      setMessage(err as string);
+    }
+  };
 
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
       <form
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          signin();
         }}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
+        <p>
+          <input
+            id="identifier"
+            onChange={(e) => setIdentifier(e.currentTarget.value)}
+            placeholder="Enter an identifier..."
+          />
+        </p>
+        <p>
+          <input
+            id="password"
+            type="password"
+            onChange={(e) => setPassword(e.currentTarget.value)}
+            placeholder="Enter an password..."
+          />
+        </p>
+        <p>
+          <button type="submit">Sign in</button>
+        </p>
       </form>
-
-      <p>{greetMsg}</p>
+      <p>{message}</p>
     </div>
   );
 }
