635c9f7a63fc4c77b27acb57faf76cbd185a30c9
sugyan <sugi1982@gmail.com>
2024-03-22 09:48:01 +0900

    Add updater downloadAndInstall

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { relaunch } from "@tauri-apps/plugin-process";
+import { check } from "@tauri-apps/plugin-updater";
+import { useEffect, useRef } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import Home from "./routes/home";
@@ -22,6 +25,18 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
+  const checking = useRef(false);
+  useEffect(() => {
+    if (checking.current) return;
+    checking.current = true;
+    (async () => {
+      const update = await check();
+      if (update) {
+        await update.downloadAndInstall();
+        await relaunch();
+      }
+    })();
+  }, []);
   return (
     <div className="dark">
       <div className="dark:text-gray-200 dark:bg-gray-800">
