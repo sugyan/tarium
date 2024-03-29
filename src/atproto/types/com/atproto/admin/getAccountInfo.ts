a8c1f42b2b677a69eb6a66ae54c0b9a7ff324463
sugyan <sugi1982@gmail.com>
2024-03-29 20:39:45 +0900

    Add ts-ignore

--- src/atproto/types/com/atproto/admin/getAccountInfo.ts
+++ src/atproto/types/com/atproto/admin/getAccountInfo.ts
@@ -1,8 +1,11 @@
 /**
  * GENERATED CODE - DO NOT MODIFY
  */
+// @ts-ignore
 import { BlobRef } from "@atproto/lexicon";
+// @ts-ignore
 import { isObj, hasProp } from "../../../../util";
+// @ts-ignore
 import { CID } from "multiformats/cid";
 import * as ComAtprotoAdminDefs from "./defs";
 
--- src/atproto/types/com/atproto/sync/subscribeRepos.ts
+++ src/atproto/types/com/atproto/sync/subscribeRepos.ts
@@ -1,8 +1,11 @@
 /**
  * GENERATED CODE - DO NOT MODIFY
  */
+// @ts-ignore
 import { BlobRef } from "@atproto/lexicon";
+// @ts-ignore
 import { isObj, hasProp } from "../../../../util";
+// @ts-ignore
 import { CID } from "multiformats/cid";
 
 /** Represents an update of repository state. Note that empty commits are allowed, which include no repo data changes, but an update to rev and signature. */
