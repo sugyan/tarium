61e06dc725c7281ccd8a1f8f22123a4ae39e6c16
sugyan <sugi1982@gmail.com>
2024-04-02 22:48:36 +0900

    Hide header in each columns

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,21 +1,24 @@
+import Column from "@/components/Column";
 import Feed from "@/components/Feed";
 import { useFeedViewPosts } from "@/hooks/useFeedViewPosts";
 import { HomeIcon } from "@heroicons/react/24/outline";
 
 const Home = () => {
   const posts = useFeedViewPosts();
-  return (
+  const headerContent = (
     <>
-      <div className="flex items-center p-2 border-b border-muted sticky top-0 bg-background z-10">
-        <div className="h-6 w-6">
-          <div className="h-6 flex justify-center items-center">
-            <HomeIcon className="h-6 w-6" />
-          </div>
+      <div className="h-6 w-6">
+        <div className="h-6 flex justify-center items-center">
+          <HomeIcon className="h-6 w-6" />
         </div>
-        <span className="ml-2 font-semibold">Home</span>
       </div>
-      <Feed posts={posts} />
+      <span className="ml-2 font-semibold">Home</span>
     </>
+  );
+  return (
+    <Column headerContent={headerContent}>
+      <Feed posts={posts} />
+    </Column>
   );
 };
 
